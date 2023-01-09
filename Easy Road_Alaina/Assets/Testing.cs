3b55d5491291853cccdb266fb7d9bc5d975763a7
amagee99 <107876195+amagee99@users.noreply.github.com>
2023-01-09 12:13:34 -0500

    Initial Commit

---
+++ Easy Road_Alaina/Assets/Testing.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Testing : MonoBehaviour
+{
+    public GameObject t;
+   
+    //public static E g;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // x.SideObjectSetActive(t, true);
+    }
+}
